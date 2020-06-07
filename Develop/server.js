6a8856d432ed04b7702716b03ef0ea567839cd55
Marcus <mpt789@gmail.com>
2020-06-06 22:36:10 -0700

    added helmet and organized code a little better, also added comments for helmet

--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,42 @@
 // Dependencies
 // =============================================================
-let express = require("express");
+//  node npms:
 let path = require("path");
 let fs = require("fs");
-const { uuid } = require("uuidv4");
-// console.log(uuid());
 
-// Sets up the Express App
-// =============================================================
+//  third party npms:
+let express = require("express");
+let { uuid } = require("uuidv4");
+let helmet = require("helmet");
+
+
 let app = express();
 let PORT = process.env.PORT || 3000;
 
+// Helmet can help protect your app from some well-known web vulnerabilities by 
+// setting HTTP headers appropriately.
+
+// Helmet is actually just a collection of smaller middleware functions that set 
+// security-related HTTP response headers:
+
+    // csp sets the Content-Security-Policy header to help prevent cross-site scripting attacks and other cross-site injections.
+    // hidePoweredBy removes the X-Powered-By header.
+    // hsts sets Strict-Transport-Security header that enforces secure (HTTP over SSL/TLS) connections to the server.
+    // ieNoOpen sets X-Download-Options for IE8+.
+    // noCache sets Cache-Control and Pragma headers to disable client-side caching.
+    // noSniff sets X-Content-Type-Options to prevent browsers from MIME-sniffing a response away from the declared content-type.
+    // frameguard sets the X-Frame-Options header to provide clickjacking protection.
+    // xssFilter sets X-XSS-Protection to enable the Cross-site scripting (XSS) filter in most recent web browsers.
+app.use(helmet());
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
 
+
+
+// API ROUTES
 app.get("/api/notes", function (req, res) {
   fs.readFile(__dirname + "/db/db.json", (err, data) => {
     if (err) throw err;
@@ -43,10 +60,6 @@ app.post("/api/notes", function (req, res) {
   });
 });
 
-// In order to delete a note, you'll need to read all
-// notes from the db.json file, remove the note with
-// the given id property, and then rewrite the notes
-// to the db.json file.
 app.delete("/api/notes/:id", function (req, res) {
   fs.readFile("db/db.json", (err, data) => {
     let db = JSON.parse(data);
@@ -58,17 +71,24 @@ app.delete("/api/notes/:id", function (req, res) {
   });
 });
 
+
+
+
+// HTML ROUTES:
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
+
+
+
+
 
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
-
-//POST - CREATE
-//GET - READ
-//PULL - UPDATE
-//DELETE - DELETE
